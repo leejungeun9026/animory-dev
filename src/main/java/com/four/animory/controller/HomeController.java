ddba63c5d568cab0c2222e10aa5c1553e394c561
dmsdl1126 <dmsdl1126@gmail.com>
2025-11-10 16:47:48 +0900

    lje style 수정 및 index에 sitterboard 추가

--- src/main/java/com/four/animory/controller/HomeController.java
+++ src/main/java/com/four/animory/controller/HomeController.java
@@ -1,15 +1,26 @@
 package com.four.animory.controller;
 
+import com.four.animory.dto.sitter.SitterBoardListDTO;
+import com.four.animory.service.sitter.SitterBoardService;
 import lombok.Getter;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
 
 @Controller
 @Log4j2
 public class HomeController {
+  @Autowired
+  private SitterBoardService sitterBoardService;
+
   @GetMapping({"/", "/index"})
-  public String index(){
-      return "index";
+  public String index(Model model){
+    List<SitterBoardListDTO> sitterBoard = sitterBoardService.getRecent(4);
+    model.addAttribute("sitterBoard", sitterBoard);
+    return "index";
   }
 }
