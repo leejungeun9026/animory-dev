981f893c03331e05352c6cd5589712001f7c2b7f
DESKTOP-40JSM55\it <hyunbingun1234@gmail.com>
2025-11-13 11:00:03 +0900

    ihb 홈 컨트롤러, 어드민 파일 삭제처리 수정

--- src/main/java/com/four/animory/controller/HomeController.java
+++ src/main/java/com/four/animory/controller/HomeController.java
@@ -2,10 +2,12 @@ package com.four.animory.controller;
 
 import com.four.animory.dto.free.FreeBoardDTO;
 import com.four.animory.dto.mate.MateBoardDTO;
+import com.four.animory.dto.notice.NoticeBoardDTO;
 import com.four.animory.dto.sitter.SitterBoardListDTO;
 import com.four.animory.dto.spr.SprBoardDTO;
 import com.four.animory.service.free.FreeService;
 import com.four.animory.service.mate.MateService;
+import com.four.animory.service.notice.NoticeService;
 import com.four.animory.service.sitter.SitterBoardService;
 import com.four.animory.service.spr.SprBoardService;
 import lombok.Getter;
@@ -28,17 +30,21 @@ public class HomeController {
   private SprBoardService sprBoardService;
   @Autowired
   private SitterBoardService sitterBoardService;
+    @Autowired
+    private NoticeService noticeService;
 
 
-  @GetMapping({"/", "/index"})
+    @GetMapping({"/", "/index"})
   public String index(Model model){
     List<FreeBoardDTO> freeBoard = freeBoardService.getTop10FreeBoardList();
     List<MateBoardDTO> mateBoard = mateBoardService.getTop10MateBoardList();
     List<SprBoardDTO> sprBoard = sprBoardService.getTop10SprBoards();
+    List<NoticeBoardDTO>noticeBoard = noticeService.getTop10NoticeBoardList();
     List<SitterBoardListDTO> sitterBoard = sitterBoardService.getRecent(4);
     log.info(mateBoard);
     model.addAttribute("freeBoard", freeBoard);
     model.addAttribute("sprBoard", sprBoard);
+    model.addAttribute("noticeBoard", noticeBoard);
     model.addAttribute("sitterBoard", sitterBoard);
     return "index";
   }
